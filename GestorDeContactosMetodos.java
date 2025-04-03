eb7b245757f01836710f7de0e92b8e52f22dacc9
Ramu21789 <2604ram@gmail.com>
2025-04-03 01:29:47 -0600

    Adjunto la parte de abad
    Ya quedo, puse la parte de abad, porque se murio su laptop

--- GestorDeContactosMetodos.java
+++ GestorDeContactosMetodos.java
@@ -81,4 +81,79 @@ static class Contacto {
                 return;
             }
         }
-        
+        System.out.println("\nNo hay un contacto con ese nombre, intentalo de nuevo.");
+    }
+
+    // metodo para guardar contactoos en un archivo
+    public static void guardarContactos() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("contactos.txt"))) {
+            for (int i = 0; i < numContactos; i++) {
+                writer.write(contactos[i].nombre + " " + contactos[i].telefono + " " + contactos[i].email + "\n");
+            }
+            System.out.println("\nSe guardaron correctamente.");
+        } catch (IOException e) {
+            System.out.println("\nError al guardar los contactos.");
+        }
+    }
+
+    // se cargan los contactos desde el archivo creado en caso de que los haya
+    public static void cargarContactos() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("contactos.txt"))) {
+            String linea;
+            numContactos = 0;
+            while ((linea = reader.readLine()) != null && numContactos < 100) {
+                String[] partes = linea.split(" ");
+                contactos[numContactos] = new Contacto(partes[0], partes[1], partes[2]);
+                numContactos++;
+            }
+            System.out.println("Contactos cargados correctamente.");
+        } catch (IOException e) {
+            System.out.println("\nNo hay contactos guardados.");
+        }
+    }
+
+    public static void main(String[] args) {
+        cargarContactos();
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("\n--- Gestor de Contactos En Java Usando Metodos ---");
+            System.out.println("1. Agregar Contacto");
+            System.out.println("2. Mostrar Contactos");
+            System.out.println("3. Buscar Contacto");
+            System.out.println("4. Eliminar Contacto");
+            System.out.println("5. Guardar Contactos");
+            System.out.println("6. Cargar Contactos");
+            System.out.println("0. Salir");
+            System.out.print("selecciona una opcion:");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    agregarContacto();
+                    break;
+                case 2:
+                    mostrarContactos();
+                    break;
+                case 3:
+                    buscarContacto();
+                    break;
+                case 4:
+                    eliminarContacto();
+                    break;
+                case 5:
+                    guardarContactos();
+                    break;
+                case 6:
+                    cargarContactos();
+                    break;
+                case 0:
+                    System.out.println("\nSaliendo del programa...");
+                    break;
+                default:
+                    System.out.println("\nSelecciona una opcion valida.");
+            }
+        } while (opcion != 0);
+    }
+}
